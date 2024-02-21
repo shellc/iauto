5a2b37730f83f348a3694b9fe37bc8b87c7b2fc4
shellc <wangshenggong@gmail.com>
2024-02-22 00:14:03 +0800

    FEATURE: auto register actions in __init__.py

--- iauto/__init__.py
+++ iauto/__init__.py
@@ -1,1 +1,15 @@
+from .actions import Playbook, PlaybookExecutor
+from .agents import _actions
+from .llms.actions import register_actions as register_llm_actions
+
 VERSION = "0.1.1"
+
+
+# Register actions
+
+register_llm_actions()
+
+__all__ = [
+    "Playbook",
+    "PlaybookExecutor"
+]
