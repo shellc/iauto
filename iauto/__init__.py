ade872b12a29f080a728f4eedfa0d2c9d7972b03
shellc <wangshenggong@gmail.com>
2024-02-24 17:55:51 +0800

    RELEASE: 0.1.2

--- iauto/__init__.py
+++ iauto/__init__.py
@@ -2,7 +2,7 @@ from .actions import Playbook, PlaybookExecutor
 from .agents import _actions
 from .llms.actions import register_actions as register_llm_actions
 
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 
 
 # Register actions
