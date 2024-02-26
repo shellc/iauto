fcfff8fb1ac4a359ede2acdaa765cae8e92f3392
shellc <wangshenggong@gmail.com>
2024-02-26 09:42:24 +0800

    FIX: export register_action function

--- iauto/actions/__init__.py
+++ iauto/actions/__init__.py
@@ -2,5 +2,5 @@ from . import _buildin
 from ._action import (Action, ActionArg, ActionSpec, Executor, Playbook,
                       create_action)
 from ._executor import PlaybookExecutor
-from ._loader import loader
+from ._loader import loader, register_action
 from ._playbook import PlaybookRunAction
