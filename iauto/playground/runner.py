3cc735a37dc1b44bdbb23d045e8422fd56dc03e3
shellc <wangshenggong@gmail.com>
2024-02-26 09:42:59 +0800

    FIX: run streamlit in python

--- iauto/playground/runner.py
+++ iauto/playground/runner.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+from streamlit.web.cli import main as streamlit_main
 
 
 def run(app, playbook_dir=None):
@@ -10,5 +13,8 @@ def run(app, playbook_dir=None):
 
     os.environ["IA_PLAYBOOK_DIR"] = playbook_dir
 
-    cmd = f"streamlit run {app_py}"
-    os.system(cmd)
+    # cmd = f"streamlit run {app_py}"
+    # os.system(cmd)
+
+    sys.argv = ["streamlit", "run", f"{app_py}"]
+    streamlit_main()
